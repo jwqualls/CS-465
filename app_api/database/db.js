ceba06bd6b83463f3ccb3d7ba6330a9d765461ee
jwqualls <jaylen.qualls@snhu.edu>
2023-09-30 10:14:59 -0500

    module5 api

--- app_api/database/db.js
+++ app_api/database/db.js
@@ -1,7 +1,7 @@
-const mongoose = require('mongoose');
-const host = process.env.DB_HOST || '127.0.0.1'
+const mongoose = require("mongoose");
+const host = process.env.DB_HOST || "127.0.0.1";
 const dbURI = `mongodb://${host}/travlr`;
-const readLine = require('redline');
+const readLine = require('readline');
 
 // avoid 'current Server Discovery and Monitoring engine is deprecated'
 mongoose.set('useUnifiedTopology', true);
@@ -65,4 +65,4 @@ process.on('SIGTERM', () => {
 connect();
 
 // bring in the Mongoose schema
-require('./models/travlr');
+require("./models/travlr");
